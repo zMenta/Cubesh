059863dd9d7f789913d3c70048c4911494247899
menta <brentano.l@gmail.com>
2023-05-26 16:59:25 -0300

    basic player movement

--- player/player.gd
+++ player/player.gd
@@ -1,16 +1,15 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
 
 func _physics_process(_delta: float) -> void:
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	if direction != Vector2():
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	look_at(get_global_mouse_position())
 	move_and_slide()
