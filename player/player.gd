4932e962757168d43351d51bd1f9fdd2553abd4b
menta <brentano.l@gmail.com>
2023-05-26 21:24:59 -0300

    better player movement

--- player/player.gd
+++ player/player.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 250
+const ACCELERATION = 30
+const FRICTION = 25
+ 
 
 func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	if direction != Vector2():
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
+		velocity.y = move_toward(velocity.y, direction.y * SPEED, ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.y = move_toward(velocity.y, 0, FRICTION)
 
 	look_at(get_global_mouse_position())
 	move_and_slide()
