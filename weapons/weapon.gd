e31a7c77e6ff25683530be7a7d1908055ebe9958
menta <brentano.l@gmail.com>
2023-05-27 11:30:07 -0300

    weapon attack type and speed_scale

--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 class_name  Weapon
 
 @export var attack : Attack
+@export_enum("thrust_attack", "swing_attack") var attack_type = "thrust_attack"
+@export var attack_speed_scale : float = 1
 @export var collision_area := Area2D
 
 
