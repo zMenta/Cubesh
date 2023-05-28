55103ac757fc7bfc6399a9f6ea7074202164239f
menta <brentano.l@gmail.com>
2023-05-28 18:34:55 -0300

    random blood splatter sprites

--- common/blood/blood.gd
+++ common/blood/blood.gd
@@ -5,6 +5,7 @@ var blood_sprites := Vector2i(4,3)
 
 
 func _ready() -> void:
+	_set_random_sprite()
 	z_index = 5
 	scale = Vector2(0.4, 0.4)
 	rotation_degrees = randf_range(0, 360)
@@ -22,5 +23,11 @@ func _ready() -> void:
 
 
 func _set_random_sprite() -> void:
-	print(region_rect.position)
+	var region_position : Vector2 = Vector2(
+		randi() % blood_sprites.x,
+		randi() % blood_sprites.y
+		) * SPRITE_SIZE
+
+	region_position += Vector2(16,64) # 16,64 is where the blood sprite cluster begins in the asset image.
+	region_rect.position = region_position
 
