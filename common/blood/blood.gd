5aa5b6ff573c59caa5ea966735225d71bfc01ef5
menta <brentano.l@gmail.com>
2023-05-28 18:17:05 -0300

    improved blood effect

--- common/blood/blood.gd
+++ common/blood/blood.gd
@@ -10,11 +10,12 @@ func _ready() -> void:
 	rotation_degrees = randf_range(0, 360)
 	var animation_time := 0.16
 	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC)
+	var final_scale = Vector2(randf_range(1, 1.5), randf_range(1, 1.5))
 
 	tween.tween_property(self, "position",
-		position + Vector2(randf_range(-20, 20), randf_range(-20, 20)),
+		position + Vector2(randf_range(15, 30), randf_range(-20, 20)),
 		animation_time)
-	tween.set_parallel(true).tween_property(self, "scale", Vector2(1,1), animation_time)
+	tween.set_parallel(true).tween_property(self, "scale", final_scale, animation_time)
 	await tween.finished
 
 	z_index = -5
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -12,6 +12,7 @@ func _on_component_health_died() -> void:
 
 
 func _on_component_health_damage_taken() -> void:
-	var new_blood: Sprite2D = blood.instantiate()
+	var new_blood: Node2D = blood.instantiate()
 	new_blood.global_position = global_position
+	new_blood.rotation = rotation + deg_to_rad(randf_range(-40, 40))
 	Gamestate.add_node_to_world.emit(new_blood)
