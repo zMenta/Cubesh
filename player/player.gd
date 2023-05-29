3f839a630aa1c977e5b6adbab87326b99cde8a1b
menta <brentano.l@gmail.com>
2023-05-29 11:27:56 -0300

    player dash move

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 const SPEED = 150
 const ACCELERATION = 20
 const FRICTION = 25
+const DASH_SPEED = 450
 
  
 func _process(_delta: float) -> void:
@@ -23,14 +24,24 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
 		velocity.y = move_toward(velocity.y, 0, FRICTION)
 
+	if Input.is_action_just_pressed("dodge"):
+		_dash(direction)
+
 	look_at(get_global_mouse_position())
 	move_and_slide()
+
+
+func _dash(direction: Vector2) -> void:
+	velocity.x = move_toward(velocity.x, direction.x * DASH_SPEED, DASH_SPEED)
+	velocity.y = move_toward(velocity.y, direction.y * DASH_SPEED, DASH_SPEED)
+	
 
 
 func _enable_weapon_collision() -> void:
 	if not current_weapon is Weapon:
 		return
 	current_weapon.enable_collision()
+
 
 func _disable_weapon_collision() -> void:
 	if not current_weapon is Weapon:
