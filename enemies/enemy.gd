1ec903107338325f0c57eef6c05d2989b107dde4
menta <brentano.l@gmail.com>
2023-05-29 12:24:55 -0300

    blood spraying inverted direction fix

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -29,7 +29,7 @@ func _on_component_health_died() -> void:
 func _on_component_health_damage_taken() -> void:
 	var new_blood: Node2D = blood.instantiate()
 	new_blood.global_position = global_position
-	new_blood.rotation = rotation + deg_to_rad(randf_range(-40, 40))
+	new_blood.rotation = rotation + deg_to_rad(randf_range(-40, 40)) + deg_to_rad(180)
 	Gamestate.add_node_to_world.emit(new_blood)
 
 
