435af925c04c0eddbb5ccbb0202c84203f19b07c
menta <brentano.l@gmail.com>
2023-06-21 11:16:38 -0300

    player interaction wip

---
+++ maps/props/map_exit.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var can_interact := false
+
+func _on_body_entered(_body:Node2D) -> void:
+	Gamestate.update_tooltip.emit("exit level")
+	can_interact = true
+
+
+func _on_body_exited(_body:Node2D) -> void:
+	Gamestate.hide_tooltip.emit()
+	can_interact = false
+
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact") and can_interact:
+		print("player interaction")
+
