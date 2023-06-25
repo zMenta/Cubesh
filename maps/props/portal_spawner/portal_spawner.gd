b44850d13fbb4b1870f8564ca9b81f1a630c0264
menta <brentano.l@gmail.com>
2023-06-25 09:52:16 -0300

    portal_spawner tweak and map adjustments

--- maps/props/portal_spawner/portal_spawner.gd
+++ maps/props/portal_spawner/portal_spawner.gd
@@ -3,9 +3,12 @@ extends Marker2D
 @export var portal_scene : PackedScene
 
 func _ready() -> void:
+	hide()
 	Gamestate.spawn_teleporter.connect(_spawn_portal)
 
 
 func _spawn_portal() -> void:
-	add_child(portal_scene.instantiate())
+	var portal := portal_scene.instantiate()
+	portal.global_position = global_position
+	Gamestate.add_node_to_world.emit(portal)
 
